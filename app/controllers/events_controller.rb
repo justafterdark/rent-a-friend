fb3a02fdaf5c14c7429363f6695edda2f75e92f4
Terence Hung <terence.hung86@gmail.com>
2019-08-27 16:19:15 +0800

    1) index, new, create event done.2) Update the event confirmation (boolean) to default as false

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_events, only: [:index]
-  before_action :set_event, only: [:show]
+  before_action :set_event, only: [:show, :edit]
 
   def index
   end
@@ -8,15 +8,25 @@ class EventsController < ApplicationController
   def show
   end
 
+  # form only
   def new
+    @event = Event.new
+    @categories = ["wedding", "birthday", "social"]
   end
 
+  # create is the action involved in creation
   def create
-    @event = Event.find(params[:id])
+    # pass strong params into Event.new
+    @event = Event.new(event_params)
+    @event.user = current_user
+    if @event.save
+      redirect_to event_path(@event)
+    else
+      render 'new'
+    end
   end
 
   def edit
-    Event.find(params[:id])
   end
 
   def update
@@ -29,6 +39,11 @@ class EventsController < ApplicationController
   end
 
   private
+
+  # add strong event params
+  def event_params
+    params.require(:event).permit(:datetime, :event_type, :description, :event_location, :meeting_location, :confirmed)
+  end
 
   def set_events
     @events = Event.all
