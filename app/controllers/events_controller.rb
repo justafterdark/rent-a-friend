c3b32e6f5cad7ca586d7ebb129c5484e055ab36e
Terence Hung <terence.hung86@gmail.com>
2019-08-27 12:10:36 +0800

    create index and show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,10 @@
 class EventsController < ApplicationController
+  before_action :set_events, only: [:index]
+  before_action :set_event, only: [:show]
+
+  def index
+  end
+
   def show
   end
 
@@ -6,14 +12,29 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.find(params[:id])
   end
 
   def edit
+    Event.find(params[:id])
   end
 
   def update
+    redirect_to event_path if @event.update(event_params)
   end
 
   def destroy
+    @event.destroy
+    redirect_to events_path
+  end
+
+  private
+
+  def set_events
+    @events = Event.all
+  end
+
+  def set_event
+    @event = Event.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,23 +5,26 @@ puts 'Destroying all existing users...'
 User.destroy_all
 puts 'Creating 20 users...'
 20.times do
-  new_user = User.new(
+  new_user = User.create(
+    email: Faker::Internet.email,
     username: Faker::Internet.username,
     password: 'password'
   )
   puts "Created user with username = #{new_user.username}"
 end
 
-puts 'Destroying all existing users...'
+puts 'Destroying all existing events...'
 Event.destroy_all
 puts 'Creating 10 events...'
 10.times do
-  Event.create(
+  event = Event.new(
     datetime: DateTime.now,
-    type: 'wedding',
+    event_type: 'wedding',
     description: 'big event',
     event_location: 'Singapore',
     meeting_location: 'Fullerton',
-    confirmed: false
+    confirmed: false,
+    user: User.all.sample
     )
+  event.save
 end
