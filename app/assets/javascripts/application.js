b9ed6a1957128060ca3612990720819f00d7e45b
Terence Hung <terence.hung86@gmail.com>
2019-08-28 16:29:54 +0800

    landing page done

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,7 @@
 //= require rails-ujs
 //= require_tree .
+$(".jumbotron").css({ height: $(window).height() + "px" });
+
+$(window).on("resize", function() {
+  $(".jumbotron").css({ height: $(window).height() + "px" });
+});
