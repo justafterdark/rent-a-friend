2f6dd2d3da9dd043523b41109f470a318d0c43ab
TJ Lee <teanjee@gmail.com>
2019-08-30 15:44:58 +0800

    pink mapbox empty

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,4 +3,6 @@ class Event < ApplicationRecord
   has_one :crowd, dependent: :destroy
   has_many :jobs, dependent: :destroy
   has_many :mobfriends, through: :jobs
+  geocoded_by :event_location
+  after_validation :geocode, if: :will_save_change_to_event_location?
 end
